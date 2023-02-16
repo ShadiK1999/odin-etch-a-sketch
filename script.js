96eda10989ba5d465dc0a8fea3532d12f9cf95a6
ShadiK1999 <123907887+ShadiK1999@users.noreply.github.com>
2023-02-16 12:28:18 +1100

    create grid and enble color change in cells

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
 const container = document.querySelector('#container');
+const button = document.querySelector('#scale');
+const errorMsg = document.querySelector('#error'); 
+
 //creating sqaure divs
-function makeRows(rows, cols) {
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
-    for (c = 0; c < (rows * cols); c++) {
+function makeRows(n) {
+    container.style.setProperty('--grid-rows', n);
+    container.style.setProperty('--grid-cols', n);
+    for (c = 0; c < (n * n); c++) {
       let cell = document.createElement("div");
       container.appendChild(cell).className = "grid-item";
     };
+    //hover over grid to change colour functionality
+    const items = document.querySelectorAll(".grid-item");
+    items.forEach(item => item.addEventListener('mouseover', (event) => { event.target.style.backgroundColor = "black";}));
   };
+
+function getScale(){
+    let scale = parseInt(prompt("Enter Grid Scale:"));
+    let error;
+    if(scale == null || scale == ""){
+      error = "please enter a number";
+    } else if(!Number.isInteger(scale)){
+      error = "invalid input, please enter a number";
+    } else {
+      if(scale > 100 || scale < 4){
+        error = "Please enter a number between 4 and 100 for a proper canvas" 
+      } else {
+      makeRows(scale);
+      }
+    }
+    errorMsg.textContent = error;
+  }
+  
+  makeRows(n = 16);
+  button.onclick = getScale;
+  
+  
+
+  
+
  
-  makeRows(16, 16);
-  const items = document.querySelectorAll(".grid-item");
-  items.forEach(item => item.addEventListener('mouseover', (event) => { event.target.style.backgroundColor = "black";}));
